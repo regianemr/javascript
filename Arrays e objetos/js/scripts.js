9efa18ac2ad766c718957ee9cf0e61bdc1552224
Regiane <regianemelo1995@gmail.com>
2024-04-17 11:48:04 -0300

    objetos, mutacao e loops array

--- Arrays e objetos/js/scripts.js
+++ Arrays e objetos/js/scripts.js
@@ -46,19 +46,88 @@
 // console.log(person.name.length);
 
 // Criando e deletando propriedades
-const car = {
-    motor: 2.0,
-    marca: "VW",
-    modelo: "Tiguana",
-    km: 20000,
-};
+// const car = {
+//     motor: 2.0,
+//     marca: "VW",
+//     modelo: "Tiguana",
+//     km: 20000,
+// };
 
-console.log(car);
+// console.log(car);
 
-car.portas = 4;
+// car.portas = 4;
 
-console.log(car);
+// console.log(car);
 
-delete car.km;
+// delete car.km;
 
-console.log(car);
+// console.log(car);
+
+
+// // Mais sobre objetos
+// const obj = {
+//     a: "teste",
+//     b: true,
+// };
+
+// console.log(obj instanceof Object);
+
+// const obj2 = {
+//     c: []
+// }
+
+// Object.assign(obj2, obj);
+
+// console.log(obj2)
+
+// // conhecendo melhor os objetos
+// console.log(Object.keys(obj));
+// console.log(Object.keys(obj2));
+// console.log(Object.keys(car));
+
+// console.log(Object.entries(car));
+
+// // Mutação
+// const a = {
+//     name: "Regiane"
+// };
+
+// const b = a
+
+// console.log(a);
+// console.log(b);
+
+// console.log(a === b);
+
+// a.age = 28;
+// console.log(a);
+// console.log(b);
+
+// delete b.age
+// console.log(a);
+// console.log(b);
+
+// Loops em arrays (listas)
+const users = ["Regi", "Vera", "Gleison", "Raquel"]
+
+for(let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`)
+}
+
+// Métodos de array: push(adiciona um item no fim da lista) e pop(adiciona no inicio da lista)
+const array = ["a", "b", "c"];
+
+array.push("d");
+
+console.log(array);
+
+console.log(array.length);
+
+array.pop()
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+console.log(array);
