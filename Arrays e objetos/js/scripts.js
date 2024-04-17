3d9221ed5138c0ed1ad5015936872b07bc24cb63
Regiane <regianemelo1995@gmail.com>
2024-04-17 15:02:33 -0300

    forEach

--- Arrays e objetos/js/scripts.js
+++ Arrays e objetos/js/scripts.js
@@ -157,16 +157,34 @@
 
 // Slice
 
-const testeSlice = ["a", "b", "c", "d", "e", "f"]
+// const testeSlice = ["a", "b", "c", "d", "e", "f"]
 
-const subArray = testeSlice.slice(2, 4);
+// const subArray = testeSlice.slice(2, 4);
 
-console.log(subArray);
-console.log(testeSlice);
+// console.log(subArray);
+// console.log(testeSlice);
 
-const subArray2 = testeSlice.slice(2, 4 + 1);
-console.log(subArray2);
+// const subArray2 = testeSlice.slice(2, 4 + 1);
+// console.log(subArray2);
 
-//slice a partir do índice "2"
-const subArray3 = testeSlice.slice(2);
-console.log(subArray3)
+// //slice a partir do índice "2"
+// const subArray3 = testeSlice.slice(2);
+// console.log(subArray3)
+
+// Método forEach - como for e while
+const nums = [1, 2, 3, 4, 5];
+
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+});
+
+const posts = [
+    {title: "Primeiro post", category: "PHP"},
+    {title: "Segundo post", category: "JavaScript"},
+    {title: "Terceiro post", category: "Python"},
+];
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+});
+
