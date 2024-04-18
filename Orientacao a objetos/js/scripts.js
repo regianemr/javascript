4811702e6654afae38dc153440a297f776d679ae
Regiane <regianemelo1995@gmail.com>
2024-04-18 11:09:15 -0300

    prototype

--- Orientacao a objetos/js/scripts.js
+++ Orientacao a objetos/js/scripts.js
@@ -1,31 +1,56 @@
-// Métodos
-const animal = {
-    nome: "Fred",
-    latir: function() {
-        console.log("au au");
-    },
-};
+// // Métodos
+// const animal = {
+//     nome: "Fred",
+//     latir: function() {
+//         console.log("au au");
+//     },
+// };
 
-console.log(animal.nome);
+// console.log(animal.nome);
 
-animal.latir();
+// animal.latir();
 
-// Aprofundando em métodos
-const pessoa = {
-    nome: "Regiane",
+// // Aprofundando em métodos
+// const pessoa = {
+//     nome: "Regiane",
 
-    getNome: function () {
-        return this.nome;
-    },
+//     getNome: function () {
+//         return this.nome;
+//     },
 
-    setNome: function(novoNome) {
-        this.nome = novoNome;
-    },
-};
+//     setNome: function(novoNome) {
+//         this.nome = novoNome;
+//     },
+// };
 
-console.log(pessoa.nome);
-console.log(pessoa.getNome());
+// console.log(pessoa.nome);
+// console.log(pessoa.getNome());
 
-pessoa.setNome("Maria");
+// pessoa.setNome("Maria");
 
-console.log(pessoa.getNome());
+// console.log(pessoa.getNome());
+
+// Prototype - espécie de herança - cadeia
+// const text = "abc"
+
+// console.log(Object.getPrototypeOf(text)) // conhecendo o ancestral do texto
+
+// const bool = true
+
+// console.log(Object.getPrototypeOf(bool));
+
+// const arr = []
+
+// console.log(Object.getPrototypeOf(arr));
+
+// // Mais sobre prototype
+// const myObject = {
+//     a: "b"
+// };
+
+// console.log(Object.getPrototypeOf(myObject));
+
+// const mySecondObject = Object.create(myObject);
+// console.log(mySecondObject);
+
+// Classes Básicas
