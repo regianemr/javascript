4b62180d33188a55636b2f958e68ec269074d2e3
Regiane <regianemelo1995@gmail.com>
2024-05-15 12:46:16 -0300

    spred operator

--- JS Moderno/js/scripts.js
+++ JS Moderno/js/scripts.js
@@ -134,3 +134,20 @@ const productDetails = {
 const {name: productName, price, category, color} = productDetails
 
 console.log(`O nome do produto é ${productName}, ele custa R$ ${price} e é da cor ${color}`)
+
+// 7 - spred operator
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+
+const a3 = [...a1, ...a2]
+console.log(a3)
+
+const a4 = [0, ...a3, 7]
+console.log(a4)
+
+const carName = {name: "BMW"};
+const carBrand = {brand: "VW"};
+const otherInfos = {km: 560000, price: 165000};
+
+const car = {...carName, ...carBrand, ...otherInfos}
+console.log(car)
