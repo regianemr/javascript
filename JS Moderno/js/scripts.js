36232bced507ab7a63ff8db223f14474d90df53f
Regiane <regianemelo1995@gmail.com>
2024-05-15 11:21:47 -0300

    destructuring

--- JS Moderno/js/scripts.js
+++ JS Moderno/js/scripts.js
@@ -1,118 +1,136 @@
-// 1 - var, let e const
+// // 1 - var, let e const
 
-var x = 10
-var y = 15
+// var x = 10
+// var y = 15
 
-if(y>10) {
-    var x = 5
-    console.log(x)
-}
+// if(y>10) {
+//     var x = 5
+//     console.log(x)
+// }
  
-console.log(x)
+// console.log(x)
 
-let a = 10
-let b = 15
+// let a = 10
+// let b = 15
 
-if (b > 10) {
-    let a = 5
-    console.log(a)
-}
+// if (b > 10) {
+//     let a = 5
+//     console.log(a)
+// }
 
-console.log(a)
+// console.log(a)
 
-function logName() {
-    const name = "Pedro"
-    console.log(name)
-}
+// function logName() {
+//     const name = "Pedro"
+//     console.log(name)
+// }
 
-const name = "Matheus"
+// const name = "Matheus"
 
-logName()
+// logName()
 
-console.log(name)
+// console.log(name)
 
-// 2- Arrow function
-const sum = function(a, b) {
-    return a + b
-}
+// // 2- Arrow function
+// const sum = function(a, b) {
+//     return a + b
+// }
 
-const arrowSum = (a, b) => a + b
+// const arrowSum = (a, b) => a + b
 
-console.log(sum(5, 5));
+// console.log(sum(5, 5));
 
-console.log(arrowSum(5, 5))
+// console.log(arrowSum(5, 5))
 
-const greeting = (name) => {
-    if(name) {
-        return `Hello, ${name}`
-    } else {
-        return "Hello"
-    }
-}
+// const greeting = (name) => {
+//     if(name) {
+//         return `Hello, ${name}`
+//     } else {
+//         return "Hello"
+//     }
+// }
 
-console.log(greeting("Regiane"))
-console.log(greeting())
+// console.log(greeting("Regiane"))
+// console.log(greeting())
 
-const user = {
-    name: "Fred",
-    sayUserName() {
-        setTimeout(function() {
-            console.log(this)
-            console.log(`Username: ${this.name}`)
-        }, 1000)
-    },
-    sayUserNameArrow() {
-        setTimeout(() => {
-            console.log(this);
-            console.log(`Username: ${this.name}`)
-        })
-    },
-};
+// const user = {
+//     name: "Fred",
+//     sayUserName() {
+//         setTimeout(function() {
+//             console.log(this)
+//             console.log(`Username: ${this.name}`)
+//         }, 1000)
+//     },
+//     sayUserNameArrow() {
+//         setTimeout(() => {
+//             console.log(this);
+//             console.log(`Username: ${this.name}`)
+//         })
+//     },
+// };
 
 // user.sayUserName()
 // user.sayUserNameArrow()
 
 // 3- Filter
-const arr = [1, 2, 3, 4, 5, 6]
+// const arr = [1, 2, 3, 4, 5, 6]
 
-const highNumbers = arr.filter((n) => {
-    if (n >= 3) {
-        return n;
-    }
-})
+// const highNumbers = arr.filter((n) => {
+//     if (n >= 3) {
+//         return n;
+//     }
+// })
 
-console.log(highNumbers)
+// console.log(highNumbers)
 
-const users = [
-    {name: "Fred", available: true},
-    {name: "Lincon", available: false},
-    {name: "Lore", available: true},
-    {name: "Maria", available: true},
-    {name: "João", available: false},
-]
+// const users = [
+//     {name: "Fred", available: true},
+//     {name: "Lincon", available: false},
+//     {name: "Lore", available: true},
+//     {name: "Maria", available: true},
+//     {name: "João", available: false},
+// ]
 
-const availableUsers = users.filter((user) => user.available)
-console.log(availableUsers)
+// const availableUsers = users.filter((user) => user.available)
+// console.log(availableUsers)
 
 // 4 - map
-const products = [
-    {name: "Camisa", price: 25.99, category: "Roupas"},
-    {name: "Chaleira", price: 199, category: "Eletro"},
-    {name: "Maçã", price: 5.99, category: "Frutas"},
-    {name: "Calça", price: 85.99, category: "Roupas"},
-    {name: "Limão", price: 2.99, category: "Frutas"}
-]
+// const products = [
+//     {name: "Camisa", price: 25.99, category: "Roupas"},
+//     {name: "Chaleira", price: 199, category: "Eletro"},
+//     {name: "Maçã", price: 5.99, category: "Frutas"},
+//     {name: "Calça", price: 85.99, category: "Roupas"},
+//     {name: "Limão", price: 2.99, category: "Frutas"}
+// ]
 
-products.map((product) => {
-    if(product.category === "Roupas") {
-        product.onSale = true
-    }
-})
+// products.map((product) => {
+//     if(product.category === "Roupas") {
+//         product.onSale = true
+//     }
+// })
 
-console.log(products)
+// console.log(products)
 
 // 5- Template literals
 const userName = "Regiane"
 const age = 29
 
 console.log(`O nome do usuário é ${userName} e ela tem ${age} anos.`)
+
+// 6 - destructuring
+const fruits = ["maçã", "mamão", "uva", "banana"]
+
+const [f1, f2, f3, f4] = fruits
+
+console.log(f2, f4)
+
+const productDetails = {
+    name: "mouse",
+    price: 45.99,
+    category: "Periférico",
+    color: "azul"
+}
+
+const {name: productName, price, category, color} = productDetails
+
+console.log(`O nome do produto é ${productName}, ele custa R$ ${price} e é da cor ${color}`)
