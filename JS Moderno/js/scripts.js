1717a38121eb679199698da69deac68b6a693696
Regiane <regianemelo1995@gmail.com>
2024-05-16 11:45:09 -0300

    revisao de classes e heranca

--- JS Moderno/js/scripts.js
+++ JS Moderno/js/scripts.js
@@ -151,3 +151,45 @@ const otherInfos = {km: 560000, price: 165000};
 
 const car = {...carName, ...carBrand, ...otherInfos}
 console.log(car)
+
+// 8 - Classes
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+    
+    productWithDiscount(discount) {
+        return this.price * (100 - discount) / 100
+    }
+}
+
+const shirt = new Product("Camisa gola v", 20);
+console.log(shirt)
+
+console.log(shirt.productWithDiscount(10))
+console.log(shirt.productWithDiscount(15))
+
+// 9 - Herança 
+class ProductWithAttributes extends Product {
+
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são: ")
+        this.colors.forEach(color => {
+            console.log(color)
+        });
+    }
+}
+
+const hat = new ProductWithAttributes("chapeu", 29.99, ["preto", "amarelo", "rosa"])
+
+console.log(hat)
+
+console.log(hat.name)
+
+hat.showColors()
