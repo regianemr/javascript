91ad0697817eb7d619ccebedbbfa166ecefe45c9
Regiane <regianemelo1995@gmail.com>
2024-05-23 13:08:26 -0300

    requisicao de post

--- Axios/js/scripts.js
+++ Axios/js/scripts.js
@@ -56,3 +56,17 @@
  }
 
  printData()
+
+ const form = document.querySelector("#post-form")
+ const titleInput = document.querySelector("#title")
+ const bodyInput = document.querySelector("#body")
+
+ form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    axios.post("https://jsonplaceholder.typicode.com/posts", {
+        title: titleInput.value, 
+        body: bodyInput.value, 
+        userId: 1,
+    })
+ })
